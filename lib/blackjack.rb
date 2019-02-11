f7efe2a3088bcaad853fba99ac8c2bd2f122a283
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 08:07:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,6 +52,8 @@ end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
@@ -60,4 +62,11 @@ end
 
 def runner
   # code runner here
+  welcome
+  cards = initial_round
+  until cards >= 21
+  cards =  hit?(cards)
+  display_card_total(cards)
+  end
+end_game(cards)
 end

ab16b3032306372a3aca83928ca5daa7030012b1
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 07:49:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,11 +4,12 @@ def welcome
 end
 
 def deal_card
-  # code #deal_card here
+ rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
